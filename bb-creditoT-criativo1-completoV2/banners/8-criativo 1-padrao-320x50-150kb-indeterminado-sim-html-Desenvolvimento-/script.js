1ec90235322a29ec985b384fb4998ccca6347c6b
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-04-27 12:19:10 -0300

    bb-creditoT-criativo1-completoV2

---
+++ bb-creditoT-criativo1-completoV2/banners/8-criativo 1-padrao-320x50-150kb-indeterminado-sim-html-Desenvolvimento-/script.js
@@ -0,0 +1,55 @@
+var animations = [
+  [
+    {
+      id: '#i01',
+      duration: 400,
+      delayBefore: 200,
+      delayAfter: 0,
+      animations: {opacity:1}
+    }, 
+    {
+      id: '#i02',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i03',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04',
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i05',
+      duration: 650,
+      delayBefore: 650,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i06',
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+ ]
+ new WAnimation(animations, {
+     loop: false,
+     clearAfterEnd: false,
+   }).startAnimation();      
