c011d650dfeac7f3abe1039ad3f93b7e7be6883e
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-07-21 19:54:49 -0300

    debrito completo v2

---
+++ debrito/especialistas2025/completo-v2/banners/9-padrão-padrao-320x480-150kb-indeterminado-nao-html-Desenvolvimento-/script.js
@@ -0,0 +1,166 @@
+var animations = [
+  [
+   {
+     id: '#i01',
+     duration: 250,
+     delayBefore: 250,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },                                                                                        
+   {
+     id: '#i02',
+     duration: 750,
+     delayBefore: 350,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },                                                                                        
+   {
+     id: '#i03',
+     duration: 750,
+     delayBefore: 500,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },                                                                                        
+   {
+     id: '#i04',
+     duration: 750,
+     delayBefore: 600,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },                                                                                        
+   {
+     id: '#i05',
+     duration: 750,
+     delayBefore: 700,
+     delayAfter: 3500,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },                                                                                        
+  ],
+  [
+   {
+     id: '#i06',
+     duration: 250,
+     delayBefore: 250,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+   {
+     id: '#i07',
+     duration: 350,
+     delayBefore: 350,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+   {
+     id: '#i08',
+     duration: 350,
+     delayBefore: 700,
+     delayAfter: 5500,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+  ],
+  [
+   {
+     id: '#i09',
+     duration: 250,
+     delayBefore: 250,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+   {
+     id: '#i10',
+     duration: 350,
+     delayBefore: 350,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+   {
+     id: '#i11',
+     duration: 350,
+     delayBefore: 700,
+     delayAfter: 6500,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },  
+  ],
+  [
+   {
+     id: '#i12',
+     duration: 250,
+     delayBefore: 250,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i13',
+     duration: 750,
+     delayBefore: 300,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i14',
+     duration: 850,
+     delayBefore: 550,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i15',
+     duration: 850,
+     delayBefore: 650,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i15-1',
+     duration: 850,
+     delayBefore: 750,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i16',
+     duration: 750,
+     delayBefore: 550,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i17',
+     duration: 550,
+     delayBefore: 600,
+     delayAfter: 0,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+   {
+     id: '#i18',
+     duration: 550,
+     delayBefore: 500,
+     delayAfter: 4500,
+     easing: "easeOutCubic",
+     animations: {opacity:1, left:0, top:0,}
+   },
+  ],
+]
+ new WAnimation(animations, {
+     loop: false,
+     clearAfterEnd: false,
+   }).startAnimation();      
