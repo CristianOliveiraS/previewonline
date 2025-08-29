56c66bddd8f9442ebf3b8113e8cab6cedbe5da3e
CristianOliveiraS <eduardoliverasuos@gmail.com>
2025-08-29 08:58:16 -0300

    mdr completo

---
+++ nacional/mdr/microcredito/completo/banners/1-CENTROOESTE-padrao-120x600-150kb-indeterminado-nao-html-Desenvolvimento-/script.js
@@ -0,0 +1,88 @@
+var animations = [
+  [ 
+    {
+      id: '#i01',
+      duration: 350,
+      delayBefore: 250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i02',
+      duration: 650,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i03',
+      duration: 650,
+      delayBefore: 550,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04',
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i05',
+      duration: 650,
+      delayBefore: 950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i06',
+      duration: 650,
+      delayBefore: 1050,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i07',
+      duration: 650,
+      delayBefore: 1150,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i08',
+      duration: 650,
+      delayBefore: 1250,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i09',
+      duration: 650,
+      delayBefore: 1350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i10',
+      duration: 650,
+      delayBefore: 1450,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+ ]
+ new WAnimation(animations, {
+     loop: false,
+     clearAfterEnd: false,
+   }).startAnimation();      
