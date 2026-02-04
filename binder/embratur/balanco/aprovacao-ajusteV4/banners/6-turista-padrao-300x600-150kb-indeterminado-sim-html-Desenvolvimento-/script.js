a9e3dfb2a9ba6f9045af6863b1bfe6e43fe9de1c
CristianOliveiraS <eduardoliverasuos@gmail.com>
2026-02-04 12:16:36 -0300

    ajustes embratur v4

---
+++ binder/embratur/balanco/aprovacao-ajusteV4/banners/6-turista-padrao-300x600-150kb-indeterminado-sim-html-Desenvolvimento-/script.js
@@ -0,0 +1,150 @@
+var animations = [
+  [ 
+    {
+      id: '#i01',
+      duration: 100,
+      delayBefore: 0,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04',
+      duration: 650,
+      delayBefore: 350,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04-1',
+      duration: 450,
+      delayBefore: 450,
+      delayAfter: 0,
+      easing: "easeOutBack",
+      animations: {opacity:1, top:0, left:0,scale: [5, 1]}
+    },
+    {
+      id: '#i04-2',
+      duration: 650,
+      delayBefore: 750,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04-3',
+      duration: 650,
+      delayBefore: 850,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04-4',
+      duration: 650,
+      delayBefore: 950,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+    {
+      id: '#i04-5',
+      duration: 650,
+      delayBefore: 1050,
+      delayAfter: 3000,
+      easing: "easeOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+  [
+     {
+      id: '#i01',
+      duration: 850,
+      delayBefore: 0,
+      delayAfter: 0,
+      easing: "easeInOutCubic",
+      animations: {opacity:1, top:"-100%", left:0}
+    },   
+    {
+      id: '#i02',
+      duration: 850,
+      delayBefore: 0,
+      delayAfter: 4000,
+      easing: "easeInOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },
+  ],
+  [
+       {
+      id: '#i02',
+      duration: 850,
+      delayBefore: 0,
+      delayAfter: 0,
+      easing: "easeInOutCubic",
+      animations: {opacity:1, top:"-100%", left:0}
+    },   
+    {
+      id: '#i03',
+      duration: 850,
+      delayBefore: 0,
+      delayAfter: 4000,
+      easing: "easeInOutCubic",
+      animations: {opacity:1, top:0, left:0}
+    },  
+  ],
+  [
+    {
+      id: '#i04',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+    {
+      id: '#i04-1',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+    {
+      id: '#i04-2',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+    {
+      id: '#i04-3',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+    {
+      id: '#i04-4',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 0,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+    {
+      id: '#i04-5',
+      duration: 250,
+      delayBefore: 100,
+      delayAfter: 400,
+      easing: "easeOutCubic",
+      animations: {opacity:0, top:0, left:0}
+    },
+  ]
+ ]
+ new WAnimation(animations, {
+     loop: true,
+     clearAfterEnd: false,
+   }).startAnimation();      
